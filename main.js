af7f10df1bfb6a1add96c821b3d39430d6ff980b
tai <fbmanhtai@gmail.com>
2022-11-04 18:28:52 +0700

    First

---
+++ main.js
@@ -0,0 +1,22 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
+
+const line = $(".tabs .line");
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+  console.log(pane);
+
+  tab.onclick = function () {
+    $(".tab-pane.active").classList.remove("active");
+
+    line.style.left = this.offsetLeft + "px";
+    line.style.width = this.offsetWidth + "px";
+
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
+});
